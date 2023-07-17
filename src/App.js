15f280a07eff2144a2de8b35429544ac1e3f82c2
mnunezsa <mnunezsa95@gmail.com>
2023-07-17 18:56:24 -0400

    update code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { initialItems } from "./constants";
+
 export default function App() {
   return (
     <div className="app">
@@ -14,15 +16,52 @@ function Logo() {
 }
 
 function Form() {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select>
+        {/* Creating a drop-down menu with 20 otpions */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..."></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item?.id || item?._id} />
+        ))}
+      </ul>
     </div>
   );
 }
-function PackingList() {
-  return <div className="list">LIST</div>;
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {/* using ternary operator to add conditional inline styles */}
+        {item.quantity} {}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
+
 function Stats() {
   return (
     <footer className="stats">
